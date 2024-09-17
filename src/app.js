2b8d3e190376fa0347a3a0ec5f6a92ffdb1c0774
pakkahwat <54430520+pakkahwat@users.noreply.github.com>
2024-09-17 13:11:19 +0700

    แก้ให้เปิดได้

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const { registerRouter, loginRouter } = require('./routers/authRouter.js');
-const { readCredentials } = require('./helpers/readCredentials.js');
-const { protectedRoutes } = require('./middleware/protectedRouter.js');
-const { createGameRouter } = require('./routers/gameRouter.js');
-const { authenticate } = require('./handlers/authHandlers.js');
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const { registerRouter, loginRouter } = require("./routers/authRouter.js");
+const { readCredentials } = require("./helpers/readCredentials.js");
+const { protectedRoutes } = require("./middleware/protectedRouter.js");
+const { createGameRouter } = require("./routers/gameRouter.js");
+const { authenticate } = require("./handlers/authHandlers.js");
 
 const createApp = (config, session, DB) => {
   const app = express();
   readCredentials(config);
 
   app.use(session);
-  if (config.ENV === 'dev') {
-    app.use(morgan('dev'));
+  if (config.ENV === "dev") {
+    app.use(morgan("dev"));
     app.use((req, res, next) => {
-		console.log(req.ip);
-		next();
-	});
+      console.log(req.ip);
+      next();
+    });
   }
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(express.static(config.PUBLIC));
+  app.use(express.static(path.join(__dirname, "..", "public")));
 
-  app.use(['/register', '/login'], authenticate);
-  app.use('/register', registerRouter(express.Router(), config));
-  app.use('/login', loginRouter(express.Router(), config));
+  app.use(["/register", "/login"], authenticate);
+  app.use("/register", registerRouter(express.Router(), config));
+  app.use("/login", loginRouter(express.Router(), config));
   app.use(protectedRoutes(createGameRouter(express.Router(), config, DB)));
 
   return app;
